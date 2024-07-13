a748bcabf375415ff1f416d09e745326ce433ea8
Art_Twis <34947496+ArtTwis@users.noreply.github.com>
2024-07-13 15:21:12 +0530

    add basic express setup

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import dotenv from "dotenv";
+import connectDb from "./db/index.js";
+import app from "./app.js";
+
+dotenv.config({
+  path: "./env",
+});
+
+connectDb()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`\n⚙️ Server is listening on port : ${process.env.PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log("MongoDB connection failed !!", error);
+  });
